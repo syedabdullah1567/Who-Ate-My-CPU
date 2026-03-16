d8957e4e109f58dd94c26f7e9e3278e5ab52f6b3
syedabdullah1567 <syedabdullah1567@gmail.com>
2026-03-16 17:47:54 +0500

    YOOHOO 2.0.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import select
 from pathlib import Path
 import time
 
@@ -37,20 +38,68 @@ def read_proc_status(pid):
         return "", "", 0
 
 
+def read_system_memory():
+    meminfo = {}
+
+    with open("/proc/meminfo") as f:
+        for line in f:
+            key, value = line.split(":")
+            meminfo[key] = int(value.split()[0])
+
+    total = meminfo["MemTotal"] // 1024
+    available = meminfo["MemAvailable"] // 1024
+
+    used = total - available
+
+    return total, used
+
+
+def groupedProcesses(processes):
+    aggregated = {}
+
+    for proc in processes:
+        name = proc["name"]
+
+        if name not in aggregated:
+            aggregated[name] = {
+                "name": name,
+                "memory": proc["memory"],
+                "cpu_usage": proc["cpu_usage"],
+                "count": 1,
+            }
+        else:
+            aggregated[name]["memory"] += proc["memory"]
+            aggregated[name]["cpu_usage"] += proc["cpu_usage"]
+            aggregated[name]["count"] += 1
+
+    return list(aggregated.values())
+
+
+def get_input_nowait():
+
+    if select.select([sys.stdin], [], [], 0)[0]:
+        return sys.stdin.readline().strip()
+    return None
+
+
 def main():
 
+    isGrouped = False
+
     while True:
+        user_input = get_input_nowait()
+        if user_input:
+            if user_input == "g" or user_input == "G":
+                isGrouped = not isGrouped
         processes = []
         total_ticks1 = read_total_cpu_ticks()
 
-        totalMemory = 0
         totalCPU = 0
 
         for entry in Path("/proc").iterdir():
             if entry.is_dir() and entry.name.isdigit():
                 pid = entry.name
                 name, state, memory = read_proc_status(pid)
-                totalMemory += memory
                 proc_ticks1 = read_proc_ticks(pid)
                 processes.append(
                     {
@@ -73,38 +122,28 @@ def main():
             proc["cpu_usage"] = (proc_delta / total_delta) * 100
             totalCPU += proc["cpu_usage"]
 
+        if isGrouped:
+            processes = groupedProcesses(processes)
+
         processes.sort(key=lambda x: x["memory"], reverse=True)
 
-        with open("processes.txt", "w") as procFile:
-            procFile.write(
-                "*                  Who-Ate-My-CPU Resource Manager                    *\n\n"
-            )
-            procFile.write("Total memory usage: " + str(totalMemory) + "\n\n")
-            procFile.write("Total CPU usage: " + str(totalCPU) + "\n\n")
-
-            for proc in processes:
-                procFile.write(f"Process {proc['pid']}:\n\n")
-                procFile.write(f"Name: {proc['name']}\n")
-                procFile.write(f"State: {proc['state']}\n")
-                procFile.write(f"Memory (MB): {proc['memory']:.2f}\n")
-                procFile.write(f"CPU Usage (%): {proc['cpu_usage']:.2f}\n\n")
-
-        print(f"File created in path {Path.cwd().name}")
+        totalMem, usedMem = read_system_memory()
 
         os.system("clear")
 
         print(
             "*                  Who-Ate-My-CPU Resource Manager                    *\n\n"
         )
-        print("Total memory usage: " + str(totalMemory) + "\n\n")
-        print("Total CPU usage: " + str(totalCPU) + "\n\n")
+        print("Total memory: " + str(totalMem) + "MB\n")
+        print("Used memory: " + str(usedMem) + "MB\n")
+        print("Total CPU usage: " + str(int(totalCPU)) + "\n\n")
 
-        print("PID\tNAME\tMEMORY\tCPU")
-        print("----------------------------")
+        print("PID\tNAME               \tMEMORY\tCPU")
+        print("-------------------------------------------")
 
         for proc in processes[:20]:
             print(
-                f"{proc['pid']}\t{proc['name']}\t{proc['memory']:.2f}\t{proc['cpu_usage']:.2f}"
+                f"{proc['pid']}\t{proc['name']}               \t{proc['memory']:.2f}\t{proc['cpu_usage']:.2f}"
             )
 
         time.sleep(1)
